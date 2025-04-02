b60565ecd165b8bf980df2ad82a47e4d45db67ef
mkssystems <77251365+mkssystems@users.noreply.github.com>
2025-04-03 00:52:01 +0200

    Update player.py

--- models/player.py
+++ models/player.py
@@ -2,14 +2,15 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
-from .base import Base  # Import Base here!
+from .base import Base
 
 class Player(Base):
     __tablename__ = "players"
+
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    game_session_id = Column(UUID(as_uuid=True), ForeignKey("game_sessions.id"))
-    player_x = Column(Integer, default=0)
-    player_y = Column(Integer, default=0)
+    game_session_id = Column(UUID(as_uuid=True), ForeignKey("game_sessions.id"), nullable=False)
+    player_x = Column(Integer, default=0, nullable=False)
+    player_y = Column(Integer, default=0, nullable=False)
     username = Column(String(100), nullable=True)
 
     game_session = relationship("GameSession", back_populates="players")
