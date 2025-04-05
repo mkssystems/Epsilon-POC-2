d116744e5641b800d8b57a529f4c07038151682e
mkssystems <77251365+mkssystems@users.noreply.github.com>
2025-04-05 02:36:02 +0200

    Update specials.py

--- models/specials.py
+++ models/specials.py
@@ -1,4 +1,3 @@
-# db/models/special.py
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from . import Base
@@ -6,9 +5,12 @@ from . import Base
 class Special(Base):
     __tablename__ = 'specials'  # Name of the table
 
-    id = Column(String, primary_key=True)
-    entity_id = Column(String, ForeignKey('entities.id'))  # Foreign key to the Entity table
-    description = Column(String)
+    id = Column(String, primary_key=True)  # Unique ID for the special
+    entity_id = Column(String, ForeignKey('entities.id'))  # Foreign key linking to 'entities' table
+    description = Column(String)  # Description of the special (consider using 'Text' if longer descriptions are needed)
 
     # Relationship to the Entity model
-    entity = relationship('Entity', back_populates='specials')
+    entity = relationship('Entity', back_populates='specials')  # Back-population to 'Entity'
+
+    def __repr__(self):
+        return f"<Special(description={self.description})>"
