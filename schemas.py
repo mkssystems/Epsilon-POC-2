97c718e068aa1f042912d303904350bbb5f06f42
mkssystems <77251365+mkssystems@users.noreply.github.com>
2025-04-07 19:45:24 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class ClientJoinRequest(BaseModel):
     client_id: str
+
+class GameSessionCreateRequest(BaseModel):
+    seed: Optional[str] = None
+    labyrinth_id: Optional[str] = None
